f708a7d16d49a379b7da13959e20371c619a2a25
Chance Nelson <cbn35@nau.edu>
2017-10-09 17:54:09 -0700

    Modified button ID's for ease of use, and added changes to the main textView based on the level buttons

--- app/src/main/java/com/cs399/bm766/cbn35/cs_399_p2_ph1/Main.java
+++ app/src/main/java/com/cs399/bm766/cbn35/cs_399_p2_ph1/Main.java
@@ -1,9 +1,6 @@
 package com.cs399.bm766.cbn35.cs_399_p2_ph1;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,6 +20,9 @@ public class Main extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView placeholderText = (TextView) findViewById(R.id.placeholderText);
+        placeholderText.setText("Main menu. Will contain simple select screen and author information");
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -70,15 +71,16 @@ public class Main extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        TextView placeholderText = (TextView) findViewById(R.id.placeholderText);
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.nav_level1) {
+            placeholderText.setText("Screen for Level 1");
+        } else if (id == R.id.nav_level2) {
+            placeholderText.setText("Screen for Level 2");
+        } else if (id == R.id.nav_level3) {
+            placeholderText.setText("Screen for Level 3");
+        } else if (id == R.id.nav_level4) {
+            placeholderText.setText("Screen for Level 4");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
